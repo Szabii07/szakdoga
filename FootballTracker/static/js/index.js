8e0df128134d81dd9e5437061848accb798860d6
Szabii07 <43721245+Szabii07@users.noreply.github.com>
2024-10-03 14:30:11 +0200

    végleges

--- FootballTracker/static/js/index.js
+++ FootballTracker/static/js/index.js
@@ -1,13 +1,15 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
+//console.log(signup_form.username.label);
 // Regisztrációs panel megjelenítése
 signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
+    
 });
 
 // Bejelentkezési panel megjelenítése
 signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
+
